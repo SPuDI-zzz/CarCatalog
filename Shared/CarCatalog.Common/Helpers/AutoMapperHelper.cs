31543b9835476c736a2d1df491c0438093e6daea
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-02-13 16:29:25 +0300

    Create Common library

---
+++ Shared/CarCatalog.Common/Helpers/AutoMapperHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace CarCatalog.Common.Helpers;
+
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+public static class AutoMappersRegisterHelper
+{
+    public static void Register(IServiceCollection services)
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(s => s.FullName != null && s.FullName.ToLower().StartsWith("dsrmyproject."));
+
+        services.AddAutoMapper(assemblies);
+    }
+}
