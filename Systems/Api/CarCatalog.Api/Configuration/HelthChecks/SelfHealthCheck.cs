d50eeff5167e75fc01153278dc3c7c2e520bc864
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-02-13 19:06:37 +0300

    Rename HealthCheck name implementation

--- Systems/Api/CarCatalog.Api/Configuration/HelthChecks/SelfHealthCheck.cs
+++ Systems/Api/CarCatalog.Api/Configuration/HelthChecks/SelfHealthCheck.cs
@@ -7,7 +7,7 @@ public class SelfHealthCheck : IHealthCheck
 {
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
-        var assembly = Assembly.Load("CarCatalog.API");
+        var assembly = Assembly.Load("CarCatalog.Api");
         var versionNumber = assembly.GetName().Version;
 
         return Task.FromResult(HealthCheckResult.Healthy(description: $"Build {versionNumber}"));
