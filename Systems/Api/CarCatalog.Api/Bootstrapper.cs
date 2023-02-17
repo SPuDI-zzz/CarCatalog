efd3abfa808761a7cfdf7848e8d991abbe87fef5
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-02-17 13:44:28 +0300

    Add bootstrapper for Api

---
+++ Systems/Api/CarCatalog.Api/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿namespace CarCatalog.Api;
+
+using CarCatalog.Services.Settings;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class Bootstrapper
+{
+    public static IServiceCollection RegisterAppServices(this IServiceCollection services)
+    {
+        services
+            .AddMainSettings()
+            .AddSwaggerSettings()
+            ;
+
+        return services;
+    }
+}
