55a87df1c6682f91e0649de629b6387909241eea
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-02-17 14:09:51 +0300

    Comment out Index

--- Systems/Api/CarCatalog.Api/Pages/Index.cshtml.cs
+++ Systems/Api/CarCatalog.Api/Pages/Index.cshtml.cs
@@ -1,8 +1,8 @@
 namespace CarCatalog.Api.Pages;
 
-//using CarCatalog.Api.Settings;
+using CarCatalog.Api.Settings;
 using CarCatalog.Common;
-//using CarCatalog.Services.Settings;
+using CarCatalog.Services.Settings;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Reflection;
@@ -10,23 +10,23 @@ using System.Reflection;
 public class IndexModel : PageModel
 {
     [BindProperty]
-    public bool OpenApiEnabled => true;//settings.Enabled;
+    public bool OpenApiEnabled => settings.Enabled;
 
     [BindProperty]
     public string Version => Assembly.GetExecutingAssembly().GetAssemblyVersion();
 
     [BindProperty]
-    public string HelloMessage => null;//apiSettings.HelloMessage;
+    public string HelloMessage => apiSettings.HelloMessage;
 
 
-    /*private readonly SwaggerSettings settings;
+    private readonly SwaggerSettings settings;
     private readonly ApiSpecialSettings apiSettings;
 
     public IndexModel(SwaggerSettings settings, ApiSpecialSettings apiSettings)
     {
         this.settings = settings;
         this.apiSettings = apiSettings;
-    }*/
+    }
 
     public void OnGet()
     {
