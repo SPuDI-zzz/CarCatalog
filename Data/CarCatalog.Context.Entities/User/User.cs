765687a0d211768727687c79781b5c5dbc2f71d5
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-03-11 00:20:35 +0300

    add field Uid and change type of PK

--- Data/CarCatalog.Context.Entities/User/User.cs
+++ Data/CarCatalog.Context.Entities/User/User.cs
@@ -1,9 +1,15 @@
 ﻿namespace CarCatalog.Context.Entities;
 
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
-public class User : IdentityUser<Guid>
+[Index("Uid", IsUnique = true)]
+public class User : IdentityUser<int>
 {
+    [Required]
+    public virtual Guid Uid { get; set; } = Guid.NewGuid();
+
     public DateOnly Birthday { get; set; }
 
     public virtual ICollection<Comment> Comments { get; set; }
