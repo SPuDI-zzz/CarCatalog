b133c5fc200c0344e6197355f13bdc3ab8167b49
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-03-24 02:58:06 +0300

    Revert "Revert "add Service Contry""
    This reverts commit 4db2580f9c6146c9b30661692abc21476b6c8c23.

---
+++ Services/CarCatalog.Services.Country/ICountryService.cs
@@ -0,0 +1,7 @@
+﻿namespace CarCatalog.Services.Country;
+
+public interface ICountryService
+{
+    Task<IEnumerable<CountryModel>> GetCountries(int offset = 0, int limit = 10);
+    Task<CountryModel> GetCountry(int countryId);
+}
