c626a8e05560b4c563a47fda14575772d314984b
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-03-24 03:03:35 +0300

    add Model Countries

---
+++ Systems/Api/CarCatalog.Api/Controllers/Countries/CountriesController.cs
@@ -0,0 +1,46 @@
+﻿namespace CarCatalog.Api.Controllers.Countries;
+
+using AutoMapper;
+using CarCatalog.Api.Controllers.Countries.Models;
+using CarCatalog.Services.Country;
+using Microsoft.AspNetCore.Mvc;
+
+[Produces("application/json")]
+[Route("api/v{version:apiVersion}/countries")]
+[ApiController]
+[ApiVersion("1.0")]
+public class CountriesController : ControllerBase
+{
+    private readonly IMapper mapper;
+    private readonly ICountryService countryService;
+
+    public CountriesController(IMapper mapper, ICountryService countryService)
+    {
+        this.mapper = mapper;
+        this.countryService = countryService;
+    }
+
+    [ProducesResponseType(typeof(IEnumerable<CountryResponse>), 200)]
+    [HttpGet("")]
+    public async Task<IEnumerable<CountryResponse>> GetCountries([FromQuery] int offset = 0, [FromQuery] int limit = 10)
+    {
+        var countries = await countryService.GetCountries(offset, limit);
+        var response = mapper.Map<IEnumerable<CountryResponse>>(countries);
+
+        return response;
+    }
+
+    /// <summary>
+    /// Get country by Id
+    /// </summary>
+    /// <response code="200">CountryResponse></response>
+    [ProducesResponseType(typeof(CountryResponse), 200)]
+    [HttpGet("{id}")]
+    public async Task<CountryResponse> GetCountryById([FromRoute] int id)
+    {
+        var countries = await countryService.GetCountry(id);
+        var response = mapper.Map<CountryResponse>(countries);
+
+        return response;
+    }
+}
---
+++ Systems/Api/CarCatalog.Api/Controllers/Countries/Models/CountryResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace CarCatalog.Api.Controllers.Countries.Models;
+
+using AutoMapper;
+using CarCatalog.Services.Country;
+
+public class CountryResponse
+{
+    /// <summary>
+    /// Country Id
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Country name
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+}
+
+public class CountryResponseProfile : Profile
+{
+    public CountryResponseProfile()
+    {
+        CreateMap<CountryModel, CountryResponse>();
+    }
+}
