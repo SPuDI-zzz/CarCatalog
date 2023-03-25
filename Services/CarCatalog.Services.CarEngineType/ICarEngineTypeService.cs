8ee2bce54d66c451c43372e8fe6f3dcd22d9e7ef
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-03-25 11:56:51 +0300

    remove enter

--- Services/CarCatalog.Services.CarEngineType/ICarEngineTypeService.cs
+++ Services/CarCatalog.Services.CarEngineType/ICarEngineTypeService.cs
@@ -4,5 +4,4 @@ public interface ICarEngineTypeService
 {
     Task<IEnumerable<CarEngineTypeModel>> GetCarEngineTypes(int offset = 0, int limit = 10);
     Task<CarEngineTypeModel> GetCarEngineType(int carEngineTypeId);
-
 }
