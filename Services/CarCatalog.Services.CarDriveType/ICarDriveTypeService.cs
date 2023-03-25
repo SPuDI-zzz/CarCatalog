3d742260a8c38aedb308b8265615bb1c7ab16f30
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-03-25 11:23:47 +0300

    rename parametr

--- Services/CarCatalog.Services.CarDriveType/ICarDriveTypeService.cs
+++ Services/CarCatalog.Services.CarDriveType/ICarDriveTypeService.cs
@@ -3,5 +3,5 @@
 public interface ICarDriveTypeService
 {
     Task<IEnumerable<CarDriveTypeModel>> GetCarDriveTypes(int offset = 0, int limit = 10);
-    Task<CarDriveTypeModel> GetCarDriveType(int countryId);
+    Task<CarDriveTypeModel> GetCarDriveType(int carDriveTypeId);
 }
