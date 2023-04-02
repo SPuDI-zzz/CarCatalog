06c6e024c49b3496dfce691c1d4ae358944ab6fb
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-04-02 21:34:03 +0300

    add MiddlewaresConfiguration

---
+++ Systems/Api/CarCatalog.Api/Configuration/MiddlewaresConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace CarCatalog.Api.Configuration;
+
+public static class MiddlewaresConfiguration
+{
+    public static void UseAppMiddlewares(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionsMiddleware>();
+    }
+}
