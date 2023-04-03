6ef548c710189f571b39dfe754bf0859163de216
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-04-03 15:36:22 +0300

    add Configurations: health check, IS4, CORS, logger

---
+++ Systems/Identity/CarCatalog.Identity/Configuration/HealthCheckConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace CarCatalog.Identity.Configuration;
+
+using CarCatalog.Common;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+public static class HealthCheckConfiguration
+{
+    public static IServiceCollection AddAppHealthChecks(this IServiceCollection services)
+    {
+        services.AddHealthChecks()
+            .AddCheck<SelfHealthCheck>("CarCatalog.Identity");
+
+        return services;
+    }
+
+    public static void UseAppHealthChecks(this WebApplication app)
+    {
+        app.MapHealthChecks("/health");
+
+        app.MapHealthChecks("/health/detail", new HealthCheckOptions
+        {
+            ResponseWriter = HealthCheckHelper.WriteHealthCheckResponse,
+            AllowCachingResponses = false,
+        });
+    }
+}
--- Systems/Identity/CarCatalog.Identity/Program.cs
+++ Systems/Identity/CarCatalog.Identity/Program.cs
@@ -1,5 +1,29 @@
+using CarCatalog.Context;
+using CarCatalog.Identity;
+using CarCatalog.Identity.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddAppLogger();
+
+var services = builder.Services;
+
+services.AddAppCors();
+
+services.AddHttpContextAccessor();
+
+services.AddAppDbContext(builder.Configuration);
+
+services.AddAppHealthChecks();
+
+services.RegisterAppServices();
+
+services.AddIS4();
+
 var app = builder.Build();
+
+app.UseAppHealthChecks();
+
+app.UseIS4();
 
 app.Run();
