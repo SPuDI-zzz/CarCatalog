2a0701e3f671c45d499799d1d92684a0ccbff02f
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-04-08 11:57:17 +0300

    add AuthConfiguration

---
+++ Systems/Api/CarCatalog.Api/Configuration/AuthConfiguration.cs
@@ -0,0 +1,71 @@
+﻿namespace CarCatalog.Api.Configuration;
+
+using CarCatalog.Common.Security;
+using CarCatalog.Context;
+using CarCatalog.Context.Entities;
+using CarCatalog.Services.Settings;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+public static class AuthConfiguration
+{
+    public static IServiceCollection AddAppAuth(this IServiceCollection services, IdentitySettings settings)
+    {
+        IdentityModelEventSource.ShowPII = true;
+
+        services
+            .AddIdentity<User, UserRole>(opt =>
+            {
+                opt.Password.RequiredLength = 0;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<MainDbContext>()
+            .AddUserManager<UserManager<User>>()
+            .AddDefaultTokenProviders();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultAuthenticateScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(IdentityServerAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.RequireHttpsMetadata = settings.Url.StartsWith("https://");
+                options.Authority = settings.Url;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = false,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    RequireExpirationTime = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+                options.Audience = "api";
+            });
+
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(AppScopes.BooksRead, policy => policy.RequireClaim("scope", AppScopes.BooksRead));
+            options.AddPolicy(AppScopes.BooksWrite, policy => policy.RequireClaim("scope", AppScopes.BooksWrite));
+        });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseAppAuth(this IApplicationBuilder app)
+    {
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        return app;
+    }
+}
