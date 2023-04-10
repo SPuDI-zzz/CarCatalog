1f6c8527b5836a52abbd4b83b3c35b45c1641b41
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-04-10 13:24:56 +0300

    add scopes: comments, cars, favorites

--- Shared/CarCatalog.Common/Security/AppScopes.cs
+++ Shared/CarCatalog.Common/Security/AppScopes.cs
@@ -2,6 +2,7 @@
 
 public static class AppScopes
 {
-    public const string BooksRead = "books_read";
-    public const string BooksWrite = "books_write";
+    public const string CarsWrite = "cars_write";
+    public const string CommentsWrite = "comments_write";
+    public const string FavoritesWrite = "favorites_write";
 }
