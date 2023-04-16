3a60540a2112fc4be09729ede2098d4611270892
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-04-17 02:12:24 +0300

    fix string args when migration without -arg

--- Data/CarCatalog.Context/Factories/DesignTimeDbContextFactory.cs
+++ Data/CarCatalog.Context/Factories/DesignTimeDbContextFactory.cs
@@ -10,7 +10,7 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<MainDbCont
 
     public MainDbContext CreateDbContext(string[] args)
     {
-        var provider = (args?[0] ?? "PostgreSQL").ToLower();
+        var provider = (args.Length > 0 ? args[0] : "PostgreSQL").ToLower();
 
         var configuration = new ConfigurationBuilder()
              .AddJsonFile("appsettings.context.json")
