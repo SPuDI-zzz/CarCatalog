63da4d35b0ec0df32bc7e5b1db7d8ea2f6097d47
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-04-16 14:12:05 +0300

    add action service

---
+++ Services/CarCatalog.Services.Actions/Action.cs
@@ -0,0 +1,21 @@
+﻿namespace CarCatalog.Services.Actions;
+
+using CarCatalog.Consts;
+using CarCatalog.Services.EmailSender;
+using CarCatalog.Services.RabbitMq;
+using System.Threading.Tasks;
+
+public class Action : IAction
+{
+    private readonly IRabbitMq rabbitMq;
+
+    public Action(IRabbitMq rabbitMq)
+    {
+        this.rabbitMq = rabbitMq;
+    }
+
+    public async Task SendEmail(EmailModel email)
+    {
+        await rabbitMq.PushAsync(RabbitMqTaskQueueNames.SEND_EMAIL, email);
+    }
+}
---
+++ Services/CarCatalog.Services.Actions/IAction.cs
@@ -0,0 +1,9 @@
+﻿namespace CarCatalog.Services.Actions;
+
+using CarCatalog.Services.EmailSender;
+using System.Threading.Tasks;
+
+public interface IAction
+{
+    Task SendEmail(EmailModel email);
+}
