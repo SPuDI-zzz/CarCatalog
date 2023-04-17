8904b6f15b36eed009cca67fa68323e17442fb2f
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-04-17 19:53:12 +0300

    add migration add_constraints_for_CarForSale_CarConfiguration_CarGeneration

--- Data/Migrations/CarCatalog.Context.MigrationsPostgreSQL/Migrations/20230417165038_add_constraints_for_CarForSale_CarConfiguration_CarGeneration.Designer.cs
+++ Data/Migrations/CarCatalog.Context.MigrationsPostgreSQL/Migrations/20230417165038_add_constraints_for_CarForSale_CarConfiguration_CarGeneration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CarCatalog.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CarCatalog.Context.MigrationsPostgreSQL.Migrations
 {
     [DbContext(typeof(MainDbContext))]
-    partial class MainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230417165038_add_constraints_for_CarForSale_CarConfiguration_CarGeneration")]
+    partial class add_constraints_for_CarForSale_CarConfiguration_CarGeneration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/CarCatalog.Context.MigrationsPostgreSQL/Migrations/20230417165038_add_constraints_for_CarForSale_CarConfiguration_CarGeneration.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarCatalog.Context.MigrationsPostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_constraints_for_CarForSale_CarConfiguration_CarGeneration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddCheckConstraint(
+                name: "\"CK_CarGeneration_YearBegin\"",
+                table: "car_generations",
+                sql: "\"YearBegin\" >= 1900 AND \"YearBegin\" <= EXTRACT(Year FROM CURRENT_DATE)");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "\"CK_CarGeneration_YearEnd\"",
+                table: "car_generations",
+                sql: "\"YearEnd\" >= \"YearBegin\" AND \"YearEnd\" <= EXTRACT(Year FROM CURRENT_DATE)");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "\"CK_CarForSale_Mileage\"",
+                table: "car_for_sales",
+                sql: "\"Mileage\" >= 0");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "\"CK_CarForSale_Price\"",
+                table: "car_for_sales",
+                sql: "\"Price\" >= 0");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "\"CK_CarConfiguration_EngineCapasity\"",
+                table: "car_configurations",
+                sql: "\"EngineCapasity\" >= 0");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "\"CK_CarConfiguration_HorsePower\"",
+                table: "car_configurations",
+                sql: "\"HorsePower\" >= 0");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "\"CK_CarConfiguration_Trunk\"",
+                table: "car_configurations",
+                sql: "\"Trunk\" >= 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "\"CK_CarGeneration_YearBegin\"",
+                table: "car_generations");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "\"CK_CarGeneration_YearEnd\"",
+                table: "car_generations");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "\"CK_CarForSale_Mileage\"",
+                table: "car_for_sales");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "\"CK_CarForSale_Price\"",
+                table: "car_for_sales");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "\"CK_CarConfiguration_EngineCapasity\"",
+                table: "car_configurations");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "\"CK_CarConfiguration_HorsePower\"",
+                table: "car_configurations");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "\"CK_CarConfiguration_Trunk\"",
+                table: "car_configurations");
+        }
+    }
+}
