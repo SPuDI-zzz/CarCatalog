908910fae542a9f20310821a20b32703b2f0d4dc
Danil <81990805+SPuDI-zzz@users.noreply.github.com>
2023-06-20 18:15:00 +0300

    add migration add_IsSold_in_CarForSale

---
+++ Data/Migrations/CarCatalog.Context.MigrationsPostgreSQL/Migrations/20230620141301_add_IsSold_in_CarForSale.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarCatalog.Context.MigrationsPostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_IsSold_in_CarForSale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSold",
+                table: "car_for_sales",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSold",
+                table: "car_for_sales");
+        }
+    }
+}
